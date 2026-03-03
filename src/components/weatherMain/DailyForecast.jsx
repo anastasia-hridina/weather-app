98e62e46977423664240d8da94eb762898ba4c6b
Anastasia Hridina <homura.nyan@gmail.com>
2026-03-03 14:38:16 +0200

    fix: improve daily forecast list layout

--- src/components/weatherMain/DailyForecast.jsx
+++ src/components/weatherMain/DailyForecast.jsx
@@ -1,30 +1,46 @@
 import PropTypes from 'prop-types';
 import { iconUrlFromCode } from '../../api/weatherApi';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import { colors } from '@mui/material';
 
 DailyForecast.propTypes = {
     title: PropTypes.string,
     items: PropTypes.array,
 };
 
+const style = {
+    py: 0,
+    width: '100%',
+};
+
 function DailyForecast({ title, items }) {
     return (
-        <div className=' bg-[#95c2e1] h-full rounded-3xl p-7 max-[1080px]:p-4 max-[992px]:p-8'>
+        <div className=' bg-[#95c2e1] h-full rounded-3xl p-6 max-[1080px]:p-4 max-[992px]:p-8'>
             <h2 className='text-cyan-100 uppercase font-semibold tracking-wide pl-1 text-sm'>{title}</h2>
-            <ul className='flex flex-col gap-4'>
-                {items.map((item) => {
+            <List sx={style}>
+                {items.map((item, index) => {
                     return (
-                        <li key={item.title} className='flex justify-between items-center gap-12 mt-8 max-[366px]:flex-col max-[366px]:gap-2'>
-                            <p className='text-cyan-50 tracking-wide text-sm'>{item.title}</p>
-                            <div className='flex items-center gap-4 max-[1080px]:flex max-[1080px]:flex-col max-[1080px]:gap-0 max-[1080px]:justify-center max-[992px]:flex-row max-[470px]:flex-col'>
-                                <img className='w-12' src={iconUrlFromCode(item.icon)} alt="" />
-                                <span className='font-bold text-white text-sm tracking-wide whitespace-nowrap max-[400px]:text-xs'>{item.description}</span>
+                        <ListItem key={item.title} disablePadding className='w-full flex flex-col'>
+                            <div className='w-full flex justify-between items-center gap-12 mt-4 max-[366px]:flex-col max-[366px]:gap-2'>
+                                <p className='text-cyan-50 tracking-wide text-sm'>{item.title}</p>
+                                <div className='flex items-center gap-4 max-[1080px]:flex max-[1080px]:flex-col max-[1080px]:gap-0 max-[1080px]:justify-center max-[992px]:flex-row max-[470px]:flex-col'>
+                                    <img className='w-12' src={iconUrlFromCode(item.icon)} alt="" />
+                                    <span className='font-bold text-white text-sm tracking-wide whitespace-nowrap max-[400px]:text-xs'>{item.description}</span>
+                                </div>
+                                <span className='font-bold text-white text-sm tracking-wide whitespace-nowrap'>{Math.ceil(item.temp.max)} <span className='text-white font-normal tracking-wide whitespace-nowrap'>/ {Math.ceil(item.temp.min)}</span></span>
                             </div>
-                            <span className='font-bold text-white text-sm tracking-wide whitespace-nowrap'>{Math.ceil(item.temp.max)} <span className='text-white font-normal tracking-wide whitespace-nowrap'>/ {Math.ceil(item.temp.min)}</span></span>
-                        </li>
+                            {index < items.length - 1 &&
+                                (
+                                    <div className="w-full h-px bg-white/25 mt-4" />
+                                )
+                            }
+                        </ListItem>
                     )
                 })}
-            </ul >
-        </div>
+            </List>
+        </div >
     );
 }
 
